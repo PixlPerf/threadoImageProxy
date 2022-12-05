e7ea10f053ecbbbf284ceb397647d0b97b2f0704
Ashwin Balaji Shekher <ashwin@threado.com>
2022-12-05 17:01:12 +0530

    additional

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var express = require("express");
 
 var app = express();
 app.use("/", proxy());
+
+var port = process.env.PORT || 3000;
+
+console.log("Server running on port", port);
+app.listen(port);
